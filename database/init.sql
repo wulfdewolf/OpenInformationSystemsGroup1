fe5d4a74d2566dfe674e7fea5948c63cf5ca708b
wulfdewolf <wolf2wulf@gmail.com>
2020-12-16 11:19:19 +0100

    Cleaned database init

--- database/init.sql
+++ database/init.sql
@@ -115,7 +115,7 @@ CREATE TABLE CoronaReading(
     newDeaths INT,
     newCured INT,
     reproductionNumber DOUBLE,
-    generatedAt TIME,
+    generatedAt DATE,
     forCity VARCHAR(40),
     providedBy VARCHAR(160),
     FOREIGN KEY (forCity) REFERENCES City(cityName), 
@@ -182,21 +182,20 @@ INSERT INTO Continent VALUES ('Asia');
 INSERT INTO Continent VALUES ('Oceania');
 INSERT INTO Continent VALUES ('Africa');
 
-INSERT INTO ContinentRestrictions VALUES ('North America', 'No more weed!');
-INSERT INTO ContinentRestrictions VALUES ('North America', 'No more beer!');
-INSERT INTO ContinentRestrictions VALUES ('Europe', 'No more smoking!');
-
+INSERT INTO ContinentRestrictions VALUES ('North America', 'Bars close at 01:00.');
+INSERT INTO ContinentRestrictions VALUES ('North America', 'Restaurant should close at 00:00.');
+INSERT INTO ContinentRestrictions VALUES ('Europe', 'No events with more than 1000 people.');
 
 
 /*
 *   Country
 */
 INSERT INTO Country VALUES ('Belgium', 'Europe');
-INSERT INTO Country VALUES ('Netherlands', 'Europe');
+INSERT INTO Country VALUES ('The Netherlands', 'Europe');
 INSERT INTO Country VALUES ('France', 'Europe');
 INSERT INTO Country VALUES ('Germany', 'Europe');
 INSERT INTO Country VALUES ('Denmark', 'Europe');
-INSERT INTO Country VALUES ('Polska', 'Europe');
+INSERT INTO Country VALUES ('Poland', 'Europe');
 INSERT INTO Country VALUES ('Greece', 'Europe');
 INSERT INTO Country VALUES ('Canada', 'North America');
 INSERT INTO Country VALUES ('USA', 'North America');
@@ -236,19 +235,24 @@ INSERT INTO NeedsVisaFor VALUES('Belgium', 'Peru');
 INSERT INTO NeedsVisaFor VALUES('France', 'China');
 INSERT INTO NeedsVisaFor VALUES('France', 'USA');
 
-INSERT INTO CanVisitFreely VALUES('Belgium', 'France');
-INSERT INTO CanVisitFreely VALUES('Belgium', 'Australia');
-INSERT INTO CanVisitFreely VALUES('Belgium', 'Netherlands');
+INSERT INTO CanVisitFreely VALUES('Belgium', 'Japan');
+INSERT INTO CanVisitFreely VALUES('Belgium', 'Morocco');
+INSERT INTO CanVisitFreely VALUES('Belgium', 'New Zealand');
+INSERT INTO CanVisitFreely VALUES('The Netherlands', 'France');
+INSERT INTO CanVisitFreely VALUES('The Netherlands', 'Belgium');
+INSERT INTO CanVisitFreely VALUES('The Netherlands', 'New Zealand');
 
 INSERT INTO NeedTest VALUES('Belgium', 'Australia');
-INSERT INTO NeedTest VALUES('France', 'Belgium');
-INSERT INTO NeedTest VALUES('Netherlands', 'Belgium');
+INSERT INTO NeedTest VALUES('The Netherlands', 'USA');
+INSERT INTO NeedTest VALUES('France', 'Australia');
+INSERT INTO NeedTest VALUES('France', 'USA');
 
 /*
 *   Region
 */
 INSERT INTO Region VALUES ('Flanders', 'Belgium');
 INSERT INTO Region VALUES ('Brussels Capital Region', 'Belgium');
+INSERT INTO Region VALUES ('Noord-Holland', 'The Netherlands');
 INSERT INTO Region VALUES ('Wallonia', 'Belgium');
 INSERT INTO Region VALUES ('Provence-Alpes-Côte d Azur', 'France');
 
@@ -261,24 +265,30 @@ INSERT INTO RegionRestrictions VALUES ('Wallonia', 'People can not go outside be
 *   City
 */
 INSERT INTO City VALUES ('Brussels', '1000', 1208542, 'Brussels Capital Region');
+INSERT INTO City VALUES ('Amsterdam', '1000', 821752, 'Noord-Holland');
 INSERT INTO City VALUES ('Namur', '5000', 110939, 'Wallonia');
 INSERT INTO City VALUES ('Marseille', '13000', 861635, 'Provence-Alpes-Côte d Azur');
 INSERT INTO City VALUES ('Nice', '6000', 344064, 'Provence-Alpes-Côte d Azur');
 
 INSERT INTO CityRestrictions VALUES ('Brussels', 'Bars and restaurants are closed.');
+INSERT INTO CityRestrictions VALUES ('Amsterdam', 'Bars, coffee shops and restaurants are closed.');
 INSERT INTO CityRestrictions VALUES ('Nice', 'Bars and restaurants are closed.');
 
 
 /*
 *   Organizations
 */
-INSERT INTO DataOrganization VALUES('https://data.gov.be');
-INSERT INTO DataOrganization VALUES('https://www.databronnencovid19.nl/');
-INSERT INTO DataOrganization VALUES('https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data');
+INSERT INTO DataOrganization VALUES('data.gov.be');
+INSERT INTO DataOrganization VALUES('www.databronnencovid19.nl');
+INSERT INTO DataOrganization VALUES('data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data');
+INSERT INTO DataOrganization VALUES('www.worldometers.info');
+INSERT INTO DataOrganization VALUES('www.fast-trackcities.org');
+INSERT INTO DataOrganization VALUES('www.coronavirus.jhu.edu');
 
-INSERT INTO TravellingOrganization VALUES('https://www.neckermann.be/');
-INSERT INTO TravellingOrganization VALUES('https://www.brusselsairlines.com/');
-INSERT INTO TravellingOrganization VALUES('https://www.ryanair.com/be/nl');
+
+INSERT INTO TravellingOrganization VALUES('www.neckermann.be');
+INSERT INTO TravellingOrganization VALUES('www.brusselsairlines.com');
+INSERT INTO TravellingOrganization VALUES('www.ryanair.com');
 
 /*
 *   Users
@@ -296,28 +306,54 @@ INSERT INTO Subscriber VALUES ('wolf.de.wulf@vub.be', 'Belgium');
 /*
 *   Connections
 */ 
-INSERT INTO AirplaneConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'https://www.neckermann.be/');
-INSERT INTO AirplaneConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'https://www.neckermann.be/');
-INSERT INTO AirplaneConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'https://www.neckermann.be/');
+INSERT INTO AirplaneConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'www.neckermann.be');
+INSERT INTO AirplaneConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'www.neckermann.be');
+INSERT INTO AirplaneConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'www.neckermann.be');
 
-INSERT INTO BusConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'https://www.neckermann.be/');
-INSERT INTO BusConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'https://www.neckermann.be/');
-INSERT INTO BusConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'https://www.neckermann.be/');
+INSERT INTO BusConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'www.neckermann.be');
+INSERT INTO BusConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'www.neckermann.be');
+INSERT INTO BusConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'www.neckermann.be');
 
-INSERT INTO TrainConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'https://www.neckermann.be/');
-INSERT INTO TrainConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'https://www.neckermann.be/');
-INSERT INTO TrainConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'https://www.neckermann.be/');
+INSERT INTO TrainConnection VALUES ('Brussels', 'Nice', '06:20:33', 84, 'www.neckermann.be');
+INSERT INTO TrainConnection VALUES ('Marseille', 'Brussels', '04:23:16', 55, 'www.neckermann.be');
+INSERT INTO TrainConnection VALUES ('Marseille', 'Nice', '01:15:10', 10, 'www.neckermann.be');
 
 /*
 *   CoronaReadings
 */
 
-INSERT INTO CoronaReading VALUES (1000, 100, 100, 0.8, '06:20:33', 'Brussels', 'https://data.gov.be');
-INSERT INTO CoronaReading VALUES (800, 600, 100, 0.7, '06:20:33', 'Marseille', 'https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data');
-INSERT INTO CoronaReading VALUES (1500, 800, 100, 0.5, '06:25:33', 'Brussels', 'https://data.gov.be');
+INSERT INTO CoronaReading VALUES (269, 5, 60, 0.8, '2020-12-01', 'Brussels', 'www.worldometers.info');
+INSERT INTO CoronaReading VALUES (198, 8, 30, 0.8, '2020-12-02', 'Brussels', 'www.worldometers.info');
+INSERT INTO CoronaReading VALUES (171, 10, 42, 0.8, '2020-12-03', 'Brussels', 'www.worldometers.info');
+INSERT INTO CoronaReading VALUES (167, 2, 20, 0.8, '2020-12-04', 'Brussels', 'www.worldometers.info');
+INSERT INTO CoronaReading VALUES (103, 4, 33, 0.8, '2020-12-05', 'Brussels', 'www.worldometers.info');
+
+INSERT INTO CoronaReading VALUES (247, 0, 40, 0.8, '2020-12-01', 'Amsterdam', 'www.fast-trackcities.org');
+INSERT INTO CoronaReading VALUES (300, 2, 15, 0.8, '2020-12-02', 'Amsterdam', 'www.fast-trackcities.org');
+INSERT INTO CoronaReading VALUES (333, 2, 56, 0.8, '2020-12-03', 'Amsterdam', 'www.fast-trackcities.org');
+INSERT INTO CoronaReading VALUES (225, 3, 34, 0.8, '2020-12-04', 'Amsterdam', 'www.fast-trackcities.org');
+INSERT INTO CoronaReading VALUES (694, 3, 120, 0.8, '2020-12-06', 'Amsterdam', 'www.fast-trackcities.org');
+
+INSERT INTO CoronaReading VALUES (230, 2, 40, 0.8, '2020-12-01', 'Nice', 'www.coronavirus.jhu.edu');
+INSERT INTO CoronaReading VALUES (146, 0,49, 0.8, '2020-12-02', 'Nice', 'www.coronavirus.jhu.edu');
+INSERT INTO CoronaReading VALUES (130, 1,35, 0.8, '2020-12-03', 'Nice', 'www.coronavirus.jhu.edu');
+INSERT INTO CoronaReading VALUES (90, 5, 14, 0.8, '2020-12-04', 'Nice', 'www.coronavirus.jhu.edu');
+INSERT INTO CoronaReading VALUES (85, 2, 20, 0.8, '2020-12-05', 'Nice', 'www.coronavirus.jhu.edu');
+
 
 
 /*
 *   SpecificTravellingAdvice
 */
-INSERT INTO SpecificTravellingAdvice VALUES ('Nit welcome.', 'Belgium', 'France');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are not welcome.', 'Belgium', 'France');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are not welcome.', 'Belgium', 'USA');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers need to provide a test to enter the country.', 'Belgium', 'Australia');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are not welcome.', 'Belgium', 'The Netherlands');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are advised not to come.', 'The Netherlands', 'France');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers need to provide a test to enter the country.', 'The Netherlands', 'USA');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are advised not to come.', 'The Netherlands', 'Belgium');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are not welcome.', 'The Netherlands', 'Australia');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers need to provide a test to enter the country.', 'France', 'Australia');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are advised not to come.', 'France', 'Belgium');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers need to provide a test to enter the country.', 'France', 'USA');
+INSERT INTO SpecificTravellingAdvice VALUES ('Travellers are advised not to come.', 'France', 'Brazil');
