73094824580e3261dacb15ecc9bb12e13738d7c4
wulfdewolf <wolf2wulf@gmail.com>
2020-12-30 22:55:51 +0100

    removed h2.sh, put it in dockerfile

--- database/h2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java \
-   -cp /var/lib/h2/h2.jar \
-   org.h2.tools.Server \
-   -web -webDaemon -webAllowOthers -webPort 8082 \
-   -tcp -tcpAllowOthers -tcpPort 9082 \
-   -baseDir /usr/lib/h2
